3f12e50e699bc7d12d3dec1f7ff2d561b5ff63e3
Dmitrii Tsyganov <sylvix.dh@gmail.com>
2018-04-02 14:59:49 +0600

    Initial commit - basic libraries installed and configured.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <header>Navbar will go here</header>
+        <main>Main content will go here</main>
+      </div>
+    );
+  }
+}
+
+export default App;
